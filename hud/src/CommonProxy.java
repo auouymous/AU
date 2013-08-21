a438e814b0b54310d0a8cff8459f53eceb106c38
auouymous <au@qzx.com>
2013-08-21 02:36:01 -0600

    moved GuiHandler into CommonProxy

--- hud/src/CommonProxy.java
+++ hud/src/CommonProxy.java
@@ -1,13 +1,31 @@
 package com.qzx.au.hud;
 
+import cpw.mods.fml.common.network.IGuiHandler;
 import cpw.mods.fml.common.network.NetworkRegistry;
 
-public class CommonProxy {
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+public class CommonProxy implements IGuiHandler {
 	public void registerEvents(){}
 	public void registerRenderers(){}
 	public void registerHandlers(){
 		// Gui Handler
-		NetworkRegistry.instance().registerGuiHandler(this, new GuiHandler());
+		NetworkRegistry.instance().registerGuiHandler(AUHud.instance, AUHud.proxy);
 	}
 	public void postInit(){}
+
+	//////////
+
+	@Override
+	public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z){
+		return null;
+	}
+
+	@Override
+	public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z){
+		if(id == 0)
+			return new GuiScreenHUD(player);
+		return null;
+	}
 }
