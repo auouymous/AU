9239355cad077c62a816a09704218a914b2c1e7d
auouymous <au@qzx.com>
2013-09-16 07:17:21 -0600

    constants for all light levels

---
+++ util/src/Light.java
@@ -0,0 +1,10 @@
+package com.qzx.au.util;
+
+public class Light {
+	public static float[] level = {
+		0.000F, 0.067F, 0.134F, 0.200F,
+		0.267F, 0.334F, 0.400F, 0.467F,
+		0.534F, 0.600F, 0.667F, 0.734F,
+		0.800F, 0.867F, 0.934F, 1.000F
+	};
+}
