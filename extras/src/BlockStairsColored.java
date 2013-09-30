d9f1bbd7099c40b13f3c3a972d76133414856550
auouymous <au@qzx.com>
2013-09-29 22:03:52 -0600

    .

--- extras/src/BlockStairsColored.java
+++ extras/src/BlockStairsColored.java
@@ -2,17 +2,10 @@ package com.qzx.au.extras;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-//import cpw.mods.fml.relauncher.Side;
-//import cpw.mods.fml.relauncher.SideOnly;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockStairs;
-//import net.minecraft.block.material.Material;
-//import net.minecraft.client.renderer.texture.IconRegister;
-//import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
-//import net.minecraft.item.ItemBlock;
-//import net.minecraft.util.Icon;
 
 import net.minecraftforge.common.MinecraftForge;
 
