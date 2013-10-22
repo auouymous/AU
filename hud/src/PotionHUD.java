a4268a727f7731981b364e657be00a99fb2499dd
auouymous <au@qzx.com>
2013-10-22 09:06:28 -0600

    fix amplifier crash, prevent future crashes

--- hud/src/PotionHUD.java
+++ hud/src/PotionHUD.java
@@ -24,40 +24,46 @@ public class PotionHUD {
 	//////////
 
 	public void draw(Minecraft mc, ScaledResolution screen, EntityPlayer player){
-		Collection potions = player.getActivePotionEffects();
-		if(potions.isEmpty()) return;
+		try {
+			Collection potions = player.getActivePotionEffects();
+			if(potions.isEmpty()) return;
 
-		int width = screen.getScaledWidth();
-		int height = screen.getScaledHeight();
-		int x = ((Cfg.potion_hud_corner&1) == 0 ? Cfg.potion_hud_x :  width-18-Cfg.potion_hud_x);
-		int y = ((Cfg.potion_hud_corner&2) == 0 ? Cfg.potion_hud_y : height-18-Cfg.potion_hud_y);
+			int width = screen.getScaledWidth();
+			int height = screen.getScaledHeight();
+			int x = ((Cfg.potion_hud_corner&1) == 0 ? Cfg.potion_hud_x :  width-18-Cfg.potion_hud_x);
+			int y = ((Cfg.potion_hud_corner&2) == 0 ? Cfg.potion_hud_y : height-18-Cfg.potion_hud_y);
 
-		GL11.glPushMatrix();
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		GL11.glDisable(GL11.GL_LIGHTING);
-		for(Iterator iterator = potions.iterator(); iterator.hasNext(); y += ((Cfg.potion_hud_corner&2) == 0 ? 20 : -20)){
-			PotionEffect effect = (PotionEffect)iterator.next();
-			Potion potion = Potion.potionTypes[effect.getPotionID()];
-			String name = StatCollector.translateToLocal(potion.getName());
-			String[] amplifiers = {"", " II", " III", " IV", " ?"};
-			int amplifier = effect.getAmplifier();
-			if(amplifier < 0) amplifier = 0;
-			else if(amplifier > amplifiers.length) amplifier = amplifiers.length;
-			name = name + amplifiers[amplifier];
-			String duration = Potion.getDurationString(effect);
+			GL11.glPushMatrix();
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			for(Iterator iterator = potions.iterator(); iterator.hasNext(); y += ((Cfg.potion_hud_corner&2) == 0 ? 20 : -20)){
+				PotionEffect effect = (PotionEffect)iterator.next();
+				Potion potion = Potion.potionTypes[effect.getPotionID()];
+				String name = StatCollector.translateToLocal(potion.getName());
+				String[] amplifiers = {"", " II", " III", " IV", " V", " VI", " VII", " VIII", " IX", " X"};
+				int amplifier = effect.getAmplifier();
+				if(amplifier < 0) amplifier = 0;
+				if(amplifier < amplifiers.length)
+					name = name + amplifiers[amplifier];
+				else
+					name = name + String.format(" %d", amplifier);
+				String duration = Potion.getDurationString(effect);
 
-			// potion icon
-			if(potion.hasStatusIcon()){
-				int icon = potion.getStatusIconIndex();
-				mc.renderEngine.bindTexture("/gui/inventory.png");
-				UI.drawTexturedModalRect(x, y,  0 + icon%8 * 18, 198 + icon/8 * 18, 18, 18, 200.0F);
+				// potion icon
+				if(potion.hasStatusIcon()){
+					int icon = potion.getStatusIconIndex();
+					mc.renderEngine.bindTexture("/gui/inventory.png");
+					UI.drawTexturedModalRect(x, y,  0 + icon%8 * 18, 198 + icon/8 * 18, 18, 18, 200.0F);
+				}
+
+				// potion name and remaining duration
+				int xx = x + ((Cfg.potion_hud_corner&1) == 0 ? 20 : -4);
+				mc.fontRenderer.drawStringWithShadow(name, xx + ((Cfg.potion_hud_corner&1) == 0 ? 0 : -mc.fontRenderer.getStringWidth(name)), y, 0xaaaaaa);
+				mc.fontRenderer.drawStringWithShadow(duration, xx + ((Cfg.potion_hud_corner&1) == 0 ? 0 : -mc.fontRenderer.getStringWidth(duration)), y+10, 0xffffff);
 			}
-
-			// potion name and remaining duration
-			int xx = x + ((Cfg.potion_hud_corner&1) == 0 ? 20 : -4);
-			mc.fontRenderer.drawStringWithShadow(name, xx + ((Cfg.potion_hud_corner&1) == 0 ? 0 : -mc.fontRenderer.getStringWidth(name)), y, 0xaaaaaa);
-			mc.fontRenderer.drawStringWithShadow(duration, xx + ((Cfg.potion_hud_corner&1) == 0 ? 0 : -mc.fontRenderer.getStringWidth(duration)), y+10, 0xffffff);
+			GL11.glPopMatrix();
+		} catch(Exception e){
+//			System.out.println("AU HUD: caught exception in potion hud");
 		}
-		GL11.glPopMatrix();
 	}
 }
