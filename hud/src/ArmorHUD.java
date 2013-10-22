da3bb92ef2ffea5880d3c041e39ce4843cae6e87
auouymous <au@qzx.com>
2013-10-22 09:06:43 -0600

    prevent future crashes

--- hud/src/ArmorHUD.java
+++ hud/src/ArmorHUD.java
@@ -95,47 +95,51 @@ public class ArmorHUD {
 	//////////
 
 	public void draw(Minecraft mc, ScaledResolution screen, EntityPlayer player){
-		GL11.glPushMatrix();
+		try {
+			GL11.glPushMatrix();
 
-		RenderItem itemRenderer = new RenderItem();
-		itemRenderer.zLevel = 200.0F;
+			RenderItem itemRenderer = new RenderItem();
+			itemRenderer.zLevel = 200.0F;
 
-		ItemStack helmet = mc.thePlayer.getCurrentArmor(3);
-		ItemStack chest = mc.thePlayer.getCurrentArmor(2);
-		ItemStack pants = mc.thePlayer.getCurrentArmor(1);
-		ItemStack boots = mc.thePlayer.getCurrentArmor(0);
-		ItemStack hand = mc.thePlayer.getHeldItem();
-		ItemStack ammo = null;
+			ItemStack helmet = mc.thePlayer.getCurrentArmor(3);
+			ItemStack chest = mc.thePlayer.getCurrentArmor(2);
+			ItemStack pants = mc.thePlayer.getCurrentArmor(1);
+			ItemStack boots = mc.thePlayer.getCurrentArmor(0);
+			ItemStack hand = mc.thePlayer.getHeldItem();
+			ItemStack ammo = null;
 
-		int width = screen.getScaledWidth();
-		int height = screen.getScaledHeight();
-		int x = ((Cfg.armor_hud_corner&1) == 0 ? Cfg.armor_hud_x :  width-16-Cfg.armor_hud_x);
-		int y = ((Cfg.armor_hud_corner&2) == 0 ? Cfg.armor_hud_y : height-80-Cfg.armor_hud_y);
+			int width = screen.getScaledWidth();
+			int height = screen.getScaledHeight();
+			int x = ((Cfg.armor_hud_corner&1) == 0 ? Cfg.armor_hud_x :  width-16-Cfg.armor_hud_x);
+			int y = ((Cfg.armor_hud_corner&2) == 0 ? Cfg.armor_hud_y : height-80-Cfg.armor_hud_y);
 
-		int nr_hand = 0, nr_ammo = -1;
-		if(hand != null){
-			nr_hand = (hand.getMaxStackSize() > 1 ? countItemsInInventory(player, hand.itemID, hand.getItemDamage()) : 1);
+			int nr_hand = 0, nr_ammo = -1;
+			if(hand != null){
+				nr_hand = (hand.getMaxStackSize() > 1 ? countItemsInInventory(player, hand.itemID, hand.getItemDamage()) : 1);
 
-			// bow ammo
-			if(hand.getItem() instanceof ItemBow){
-				nr_ammo = countItemsInInventory(player, Item.arrow.itemID, 0);
-				ammo = new ItemStack(Item.arrow);
+				// bow ammo
+				if(hand.getItem() instanceof ItemBow){
+					nr_ammo = countItemsInInventory(player, Item.arrow.itemID, 0);
+					ammo = new ItemStack(Item.arrow);
+				}
+
+				// move HUD up if at bottom and has ammo
+				if(ammo != null) y -= ((Cfg.armor_hud_corner&2) == 0 ? 0 : 16);
 			}
 
-			// move HUD up if at bottom and has ammo
-			if(ammo != null) y -= ((Cfg.armor_hud_corner&2) == 0 ? 0 : 16);
+			GL11.glTranslatef(0.0F, 0.0F, 32.0F);
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+
+			this.drawItemStack(mc, itemRenderer, helmet, x, y, 0, false);
+			this.drawItemStack(mc, itemRenderer, chest, x, y+16, 0, false);
+			this.drawItemStack(mc, itemRenderer, pants, x, y+32, 0, false);
+			this.drawItemStack(mc, itemRenderer, boots, x, y+48, 0, false);
+			this.drawItemStack(mc, itemRenderer, hand, x, y+64, nr_hand, false);
+			this.drawItemStack(mc, itemRenderer, ammo, x, y+80, nr_ammo, true);
+
+			GL11.glPopMatrix();
+		} catch(Exception e){
+//			System.out.println("AU HUD: caught exception in armor hud");
 		}
-
-		GL11.glTranslatef(0.0F, 0.0F, 32.0F);
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-
-		this.drawItemStack(mc, itemRenderer, helmet, x, y, 0, false);
-		this.drawItemStack(mc, itemRenderer, chest, x, y+16, 0, false);
-		this.drawItemStack(mc, itemRenderer, pants, x, y+32, 0, false);
-		this.drawItemStack(mc, itemRenderer, boots, x, y+48, 0, false);
-		this.drawItemStack(mc, itemRenderer, hand, x, y+64, nr_hand, false);
-		this.drawItemStack(mc, itemRenderer, ammo, x, y+80, nr_ammo, true);
-
-		GL11.glPopMatrix();
 	}
 }
