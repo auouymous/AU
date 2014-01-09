c2aad9f4f6660f8667ac57371caa9eb22b9f15d2
auouymous <au@qzx.com>
2014-01-08 21:07:24 -0700

    added world and coords to get tile entity

--- core/src/ISidedBlock.java
+++ core/src/ISidedBlock.java
@@ -1,5 +1,7 @@
 package com.qzx.au.core;
 
+import net.minecraft.world.World;
+
 public interface ISidedBlock {
-	public abstract void toggleSide(int side);
+	public abstract void toggleSide(World world, int x, int y, int z, int side);
 }
