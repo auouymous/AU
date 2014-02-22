7a2e2877a8f9f2979acf6499e17f6e723771cca9
auouymous <au@qzx.com>
2014-02-22 01:42:08 -0700

    fixed server side issue

--- extras/src/TickHandlerServerPlayer.java
+++ extras/src/TickHandlerServerPlayer.java
@@ -2,8 +2,6 @@ package com.qzx.au.extras;
 
 import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.TickType;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -11,7 +9,6 @@ import net.minecraft.item.ItemStack;
 
 import java.util.EnumSet;
 
-@SideOnly(Side.CLIENT)
 public class TickHandlerServerPlayer implements ITickHandler {
 	@Override
 	public void tickStart(EnumSet<TickType> type, Object... tickData){
