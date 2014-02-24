6aa547006acc458539d4a64b72fd46e0f07b8942
auouymous <au@qzx.com>
2014-02-23 23:02:14 -0700

    renamed textures for stone half slabs

--- extras/xpms/convert-vanilla-textures.sh
+++ extras/xpms/convert-vanilla-textures.sh
@@ -42,8 +42,8 @@ function colorize_blocks(){
 #convert -colors 64 rf_composite.xpm items/cookedFlesh.xpm
 
 # blocks
-rm -f blocks/colorStoneSlabTop*
-rm -f blocks/colorStoneSlabSide*
+rm -f blocks/colorStoneHalfSlabTop*
+rm -f blocks/colorStoneHalfSlabSide*
 rm -f blocks/colorCobble*
 rm -f blocks/colorStone*
 rm -f blocks/colorStoneBrick*
@@ -53,8 +53,8 @@ rm -f blocks/colorGravel*
 rm -f blocks/colorSand*
 rm -f blocks/colorFlower*
 rm -f blocks/colorIronBars*
-colorize_blocks vanilla-stone_slab_top.png colorStoneSlabTop
-colorize_blocks vanilla-stone_slab_side.png colorStoneSlabSide
+colorize_blocks vanilla-stone_slab_top.png colorStoneHalfSlabTop
+colorize_blocks vanilla-stone_slab_side.png colorStoneHalfSlabSide
 colorize_blocks vanilla-cobblestone.png colorCobble
 colorize_blocks vanilla-stone.png colorStone
 colorize_blocks vanilla-stonebrick.png colorStoneBrick
